73ff3fca7ba6afff3f408d4651fff19664f5ece5
Vadim <mazhitov.vadim@gmail.com>
2022-07-22 13:25:23 +0300

    Add solution for MedianOfTwoSortedArrays problem

---
+++ LeetCodeSolutions.Tests/HardProblemsTests.cs
@@ -0,0 +1,16 @@
+namespace LeetCodeSolutions.Tests;
+
+using LeetCodeSolutions.Solutions;
+
+public class HardProblemsTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 3 }, new[] { 2 }, 2.0)]
+    [InlineData(new[] { 1, 2 }, new[] { 3, 4 }, 2.5)]
+    public void TestMedianOfTwoSortedArrays(int[] nums1, int[] nums2, double expectedMedian)
+    {
+        var resultMedian = MedianOfTwoArraysSolution.FindMedianOfTwoSortedArrays(nums1, nums2);
+
+        resultMedian.Should().Be(expectedMedian);
+    }
+}
---
+++ LeetCodeSolutions/Solutions/MedianOfTwoArraysSolution.cs
@@ -0,0 +1,31 @@
+namespace LeetCodeSolutions.Solutions;
+
+public static class MedianOfTwoArraysSolution
+{
+    public static double FindMedianOfTwoSortedArrays(int[] nums1, int[] nums2)
+    {
+        _ = nums1.TryGetNonEnumeratedCount(out var nums1Count);
+        _ = nums2.TryGetNonEnumeratedCount(out var nums2Count);
+        var countOfNums = nums1Count + nums2Count;
+
+        var mergedNums = new int[countOfNums];
+        nums1.CopyTo(mergedNums, 0);
+        nums2.CopyTo(mergedNums, nums1Count);
+        Array.Sort(mergedNums);
+
+        if (countOfNums % 2 == 0)
+        {
+            var firstIndex = countOfNums / 2;
+            var secondIndex = firstIndex - 1;
+            double firstNum = mergedNums[firstIndex];
+            double secondNum = mergedNums[secondIndex];
+
+            return (firstNum + secondNum) / 2.0;
+        }
+
+        var midIndex = countOfNums / 2;
+        double median = mergedNums[midIndex];
+
+        return median;
+    }
+}
